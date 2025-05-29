fd7b5870e3df14f9386470d917e3a3416904eeb6
Michele Savino <michelesavino003@gmail.com>
2025-05-29 16:29:44 +0200

    Aggiunto button

---
+++ button.py
@@ -0,0 +1,22 @@
+import machine
+import utime
+
+class Button:
+    
+    def __init__(self, pin, debounce_ms=200):
+        self.pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP)
+        self.debounce_ms = debounce_ms
+        self.last_press_time = 0
+        
+    def is_pressed(self):
+        return self.pin.value() == 0
+    
+    def was_clicked(self):
+        if self.is_pressed():
+            current_time = utime.ticks_ms()
+            if utime.ticks_diff(current_time, self.last_press_time) > self.debounce_time: #se il tempo passato dal preceente click è maggiore del debounce
+                self.last_press_time = current_time
+                return True
+        return False
+        
+    
