be413278bbcbb921fd93835bed2fe4eb6cc6ddce
Mauro03av <m.melillo34@studenti.unisa.it>
2025-05-30 14:23:13 +0200

    modofica button e main , main da testare

--- button.py
+++ button.py
@@ -11,12 +11,20 @@ class Button:
     def is_pressed(self):
         return self.pin.value() == 0
     
-    def was_clicked(self):
+    def set_irq(self,step,client,topic):
+        self.pin.irq(handler=self.was_clicked(step,client,topic), trigger=self.pin.IRQ_FALLING)
+    
+    def was_clicked(self, step, client, topic_pub):
         if self.is_pressed():
             current_time = utime.ticks_ms()
             if utime.ticks_diff(current_time, self.last_press_time) > self.debounce_time: #se il tempo passato dal preceente click è maggiore del debounce
                 self.last_press_time = current_time
+                step.return_to_home()
+                client.publish(topic_pub,"Ritorno in posizione di start")
                 return True
         return False
         
+
     
+    
+
